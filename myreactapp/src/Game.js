20a7041373cbabc34e4098214c708e77e482b36c
Amirali Azim <aazimita@uwaterloo.ca>
2022-10-18 15:16:19 -0400

    stable cell marking

--- myreactapp/src/Game.js
+++ myreactapp/src/Game.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './Game.css';
+import Cell from './Cell';
+
 function Game() {
     const CELL_SIZE = 20;
     const WIDTH_SIZE = 800;
@@ -7,13 +9,20 @@ function Game() {
     const WIDTH = 600;
     const rows = HEIGHT / CELL_SIZE;
     const cols = WIDTH / CELL_SIZE;
-    let cells = useState(true);
+    let [cells, setCells] = useState([]);
+    let boardRef = useRef([]);
+    let [board, setBoard] = useState([]);
+    console.log("board reset called: ", board);
+
+    useEffect(() => {
+        setBoard(makeEmptyBoard)
+    }, []);
 
     function makeEmptyBoard() {
         let board = [];
         for (let y=0; y<rows; y++){
             board[y] = [];
-            for (let x=0; x < this.cols; x++){
+            for (let x=0; x < cols; x++){
                 board[y][x] = false;
             }
         }
@@ -33,7 +42,25 @@ function Game() {
     }
 
     function getElementOffset(){
+        const rect = boardRef.current.getBoundingClientRect();
+        const doc = document.documentElement;
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) - doc.clientTop,
+        };
+    }
 
+    function handleClick(event){
+        const elemOffset = getElementOffset();
+        const offsetX = event.clientX - elemOffset.x;
+        const offsetY = event.clientY - elemOffset.y;
+        const x = Math.floor(offsetX / CELL_SIZE);
+        const y = Math.floor(offsetY / CELL_SIZE);
+        if (x >= 0 && x <= cols && y >= 0 && y <= rows) {
+            board[y][x] = !board[y][x];
+        }
+
+        setCells(makeCells(board));
     }
 
 
@@ -44,7 +71,15 @@ function Game() {
                 width: WIDTH, 
                 height: HEIGHT,
                 backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px` 
-            }}> 
+            }}
+            onClick={(event) => handleClick(event)}
+            ref={boardRef}
+            > 
+            {
+                cells.map(cell => (
+                    <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`}/>
+                ))
+            }
             </div>      
         </div>    
     );
